e241b32b11d5aa10ef70952776cf08a4509fbad0
Thiago F <thiagofern4ndo@gmail.com>
2025-02-17 18:09:04 -0300

    novo exercicio

--- lib/logicaMatematica/logica8.dart
+++ lib/logicaMatematica/logica8.dart
@@ -1,36 +1,13 @@
 import 'dart:io';
 
-int digitalRoot(int n) {
+void main () {
 
-  while (n >= 10) {
+print('Digite um Numero');
+  int? valor = int.tryParse(stdin.readLineSync()!);
 
-    int sum = 0;
-
-    while (n > 0) {
-     
-      sum += n % 10; 
-      n ~/= 10; 
-    }
-    n = sum; 
-  }
-  
- 
-  return n; 
-}
-
-void main() {
-
-  print('Digite um número não negativo:');
-  
-  String? input = stdin.readLineSync(); 
-  
-
-  if (input != null) { 
-    int? number = int.tryParse(input); 
-    if (number != null && number >= 0) {
-      print('A raiz digital de $number é: ${digitalRoot(number)}');
-    } else {
-      print('Por favor, insira um número não negativo válido.');
-    }
+  if (valor! > 10) {
+print("O Valor é maior que 10");
+  } else { 
+    print("O Valor é menor que 10");
   }
 }
